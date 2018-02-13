305d5d63c7682458762481680ac5275ef6a85064
coreyjadams <coreyadams@fas.harvard.edu>
2018-02-13 15:26:04 -0600

    Update data interface for new pmap model.

--- invisible_cities/viewer/manager/IOManager.py
+++ invisible_cities/viewer/manager/IOManager.py
@@ -1,6 +1,11 @@
 import os
 
-from invisible_cities.io import pmap_io, mchits_io
+import numpy
+
+from invisible_cities.io import pmaps_io
+from invisible_cities.io import run_and_event_io
+from invisible_cities.io import mchits_io
+
 
 class IOManager(object):
     """wrapper to IC event interface to allow random access through events
@@ -12,14 +17,10 @@ class IOManager(object):
     def __init__(self):
         super(IOManager, self).__init__()
 
-        # List of event numbers
-        self._events = []
+
         # Current entry in the above list
-        self._entry = 0
-        # Highest entry available
-        self._max_entry = 0
-        # Current run (informational)
-        self._run = 0
+        self._current_entry = 0
+
 
     def event(self):
         """Get the data from the current event
@@ -27,7 +28,7 @@ class IOManager(object):
         Returns:
             [type] -- [description]
         """
-        return self._events[self._entry]
+        return self._events['evt_number'][self._current_entry]
 
     def entry(self):
         """Get the currently accessed entry
@@ -35,7 +36,7 @@ class IOManager(object):
         Returns:
             int -- the active entry
         """
-        return self._entry
+        return self._current_entry
 
     def run(self):
         """Get the run number of the current entry
@@ -43,7 +44,26 @@ class IOManager(object):
         Returns:
             int -- the current run number
         """
-        return self._run
+        return self._runs['run_number'][self._current_entry]
+
+    def timestamp(self):
+        """Get the timestamp for the current entry
+
+        Returns:
+            timestamp
+        """
+        return self._events['timestamp'][self._current_entry]
+
+
+    def run_and_event_read(self, file_name):
+        """Read the run and event info from the files
+
+        """
+        return run_and_event_io.read_run_and_event(file_name)
+
+    def pmaps_read(self, file_name):
+
+        return pmaps_io.load_pmaps(file_name)
 
     def set_file(self, file_name):
         """Open a new file and read it's data
@@ -57,11 +77,22 @@ class IOManager(object):
             file_name {str} -- path to file to open
         """
 
+        self._runs, self._events = self.run_and_event_read(file_name)
+
+
+        self._entries = numpy.arange(0, len(self._runs))
+
+
         # Load the pmaps, catch exception by declaring the presence of pmaps as false
         try:
-            self._s1_dict, self._s2_dict, self._s2si_dict = pmap_io.load_pmaps(file_name)
+            # print(pmaps_io.load_pmaps(file_name).keys())
+            # pmaps dict is a dictionary of PMap objects (evm.pmaps.PMap)
+            # indexed by event number
+            self._pmap_dict = self.pmaps_read(file_name)
+
             self._has_pmaps = True
-        except:
+        except Exception as e:
+            print(e)
             self._has_pmaps = False
 
         # Load MC information, catch exception by declaring the presence of mc info as false
@@ -88,76 +119,31 @@ class IOManager(object):
         if not (self._has_reco or self._has_pmaps or self._has_mc):
             raise Exception("Couldn't load file {}.".format(file_name))
 
-        # Use the S2_dict as the list of events.
-        # This explicitly requires that events have both s2 and s2si available.
-        self._events = list(set(self._s1_dict.keys()).intersection(set(self._s2si_dict.keys())))
-        # Store the highest available entry to access
-        self._max_entry = len(self._events) -1
 
     def has_pmaps(self):
         return self._has_pmaps
 
-    def s1(self, event=-1):
-        """Return s2 information for an event
+    def pmap(self):
+        """Return the pmap object for selected entry
 
         If event is specified explicitly, check event is available and return that s2
         Otherwise, return s2 for currently active event
 
         Keyword Arguments:
-            event {number} -- event number (default: {-1})
+            entry {number} -- [event number] (default: {-1})
 
         Returns:
-            S1 -- S1 object of PMap
+            PMap - evm.Pmap object
         """
+
         if not self._has_pmaps:
             return None
-        if event == -1:
-            event = self._events[self._entry]
-        if event not in self._events:
-            print("Can't go to event {}".format(event))
-        return self._s1_dict[event]
 
-    def s2(self, event=-1):
-        """Return s2 information for an event
+        event = self.event()
+        return self._pmap_dict[event]
 
-        If event is specified explicitly, check event is available and return that s2
-        Otherwise, return s2 for currently active event
 
-        Keyword Arguments:
-            event {number} -- event number (default: {-1})
-
-        Returns:
-            S2 -- S2 object of PMap
-        """
-        if not self._has_pmaps:
-            return None
-        if event == -1:
-            event = self._events[self._entry]
-        if event not in self._events:
-            print("Can't go to event {}".format(event))
-        return self._s2_dict[event]
-
-    def s2si(self, event=-1):
-        """Return s2si information for an event
-
-        If event is specified explicitly, check event is available and return that s2si
-        Otherwise, return s2si for currently active event
-
-        Keyword Arguments:
-            event {number} -- event number (default: {-1})
-
-        Returns:
-            s2si -- s2si object of PMap
-        """
-        if not self._has_pmaps:
-            return None
-        if event == -1:
-            event = self._events[self._entry]
-        if event not in self._events:
-            print("Can't go to event {}".format(event))
-        return self._s2si_dict[event]
-
-    def mchits(self, event=-1):
+    def mchits(self):
         """Return mchit objects
 
         If event is specified explicitly, check event is available and return that mchits
@@ -169,13 +155,13 @@ class IOManager(object):
         Returns:
             mchits -- MCHits object
         """
+
+
         if not self._has_mc:
             print("This file does not have mc information.")
             return None
-        if event == -1:
-            event = self._events[self._entry]
-        if event not in self._events:
-            print("Can't go to event {}".format(event))
+
+        event = self.event()
         return self._mc_hits[event]
 
     def mctracks(self, event=-1):
@@ -193,10 +179,7 @@ class IOManager(object):
         if not self._has_mc:
             print("This file does not have mc information.")
             return None
-        if event == -1:
-            event = self._events[self._entry]
-        if event not in self._events:
-            print("Can't go to event {}".format(event))
+        event = self.event()
         return self._mc_part[event]
 
     def get_num_events(self):
@@ -216,8 +199,8 @@ class IOManager(object):
         Arguments:
             entry {int} -- Desired entry
         """
-        if entry >= 0 and entry < self.get_num_events():
-            self._entry = entry
+        if entry in self._entries:
+            self._current_entry = entry
         else:
             print("Can't go to entry {}, entry is out of range.".format(entry))
 
